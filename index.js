5a08453e457d1b01e15e715c5f3d87c5ab45d022
Ashraf Shahin <ashraf.hossein.web@gmail.com>
2026-04-03 06:50:44 +0100

    nodemon added and port error solved...

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 console.log(process.env.PORT);
 
 const port = process.env.PORT || 5000;
-app.listen(5000, () => {
+app.listen(port, () => {
     console.log(`Server Running on port ${port}`);
 
 });
