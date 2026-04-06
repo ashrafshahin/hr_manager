f596203cbd1f303c92777f4d9f831b9a29d59ba3
Ashraf Shahin <ashraf.hossein.web@gmail.com>
2026-04-06 18:52:50 +0100

    create new user, existing user blocking, error handling user side 400 and server side 500, please note: postman can block password by res.send() not writing password, but for database we have to use bcrypt middleware system...

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,18 +1,33 @@
 const User = require("../models/userSchema")
 
-const registrationController = (req, res) => {
+const registrationController = async (req, res) => {
     const { username, email, password } = req.body
-    // res.send('Assalamu Alikum, HR Management Project Testing...')
+    // Check existing user ...
+    try {
+        const existingUser = await User.findOne({ email: email });
+        if (existingUser) {
+            return res.status(400).json({ success: false, message: 'Email already exists...' })
+        };
+        // Creating new users...
+        const createUser = new User({
+            username: username,
+            email: email,
+            password: password,
+        })
+        // .save() bracket er por o deya jai..
+        createUser.save();
+        res.send({
+            id: createUser._id,
+            username: createUser.username,
+            email: createUser.email,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ success: false, message: 'Server Error...' })
+        
+    };
+
     
-    // Creating new users...
-    const createUser = new User({
-        username: username,
-        email: email,
-        password: password,
-    })
-    // .save() bracket er por o deya jai..
-    createUser.save();
-    res.send(createUser);
 }
 
 module.exports = { registrationController }
