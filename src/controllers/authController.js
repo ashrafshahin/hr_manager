97c977b74f7488d3725f84ab203769b2b180b818
Ashraf Shahin <ashraf.hossein.web@gmail.com>
2026-04-06 17:46:30 +0100

    create userSchema for registration and it is a one kind of validation process, create a new User in the database... basic ideas...

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,21 @@
+const User = require("../models/userSchema")
 
 const registrationController = (req, res) => {
     const { username, email, password } = req.body
-    console.log(username, email, password);
-    res.send({username, email, password})
+    // res.send('Assalamu Alikum, HR Management Project Testing...')
     
-    res.send('Assalamu Alikum, HR Management Project Testing...')
-     
+    // Creating new users...
+    const createUser = new User({
+        username: username,
+        email: email,
+        password: password,
+    })
+    // .save() bracket er por o deya jai..
+    createUser.save();
+    res.send(createUser);
 }
 
-module.exports = {registrationController}
+module.exports = { registrationController }
+
+//note:
+// সংক্ষেপে বলি 👉 না, এক request-এ দুইটা res.send() ব্যবহার করা যায় না ❌
---
+++ src/models/userSchema.js
@@ -0,0 +1,41 @@
+const { type } = require('express/lib/response')
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required..."]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required..."],
+        trim: true,
+        lowercase: true,
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required..."],
+        min: [8, 'minimum 8 digit required...'],
+        max: [14, 'You exceeded maximum limit...'],
+        match: [/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,14}$/, 'Please fill a valid Password...']
+    },
+    photo:{
+        type: String
+    },
+    nid: {
+        type: Number,
+        min: [10, 'Smart card - minimum 10 digit required...'],
+        max: [17, 'Un Smart card - maximum 17 digit required...']
+    },
+    address: {
+        type: String,
+    }
+     
+})
+
+module.exports = mongoose.model('User', userSchema);
+
+// model hobe its a controllers, schema is a blueprint...  x mongoose.schema x √ mongoose.model √
